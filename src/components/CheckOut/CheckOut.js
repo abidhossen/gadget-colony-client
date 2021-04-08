270afa897d5168b0b962af90c44442b8c235a881
abidhossain15 <abidhossain3000@gmail.com>
2021-04-08 09:17:35 +0600

    cors

--- src/components/CheckOut/CheckOut.js
+++ src/components/CheckOut/CheckOut.js
@@ -30,7 +30,7 @@ const CheckOut = () => {
     };
     const handleOrder=() =>{
         
-       const newOrder= {...loggedInUser,...selectedDate}
+       const newOrder= {...loggedInUser,...selectedDate,...productInfo}
        fetch('https://cors-anywhere.herokuapp.com/https://tranquil-cliffs-66527.herokuapp.com/addOrder',{
            method:'POST',
            headers: {'Content-Type': 'application/json'},
